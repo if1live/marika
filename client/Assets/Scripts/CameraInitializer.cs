6c8e61289b4e3a364482f66b4284cf208e323964
if1live <libsora25@gmail.com>
2014-09-22 15:13:39 +0900

    unity vs 2013 카메라 자동 초점조절

---
+++ client/Assets/Scripts/CameraInitializer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraInitializer : MonoBehaviour {
+
+	void Start () {
+	    // camera
+        bool focusModeSet = CameraDevice.Instance.SetFocusMode(CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+        if (!focusModeSet)
+        {
+            Debug.Log("Failed to set focus mode (unsupported mode).");
+        }
+	}
+}
